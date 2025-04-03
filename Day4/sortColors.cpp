6e5b9e1bcfbe796882b6fc329122c79e2ad361fe
amitsevda2303 <rohitkmt88@gmail.com>
2025-04-03 08:43:57 +0530

    change program according to the leetcode

--- Day4/sortColors.cpp
+++ Day4/sortColors.cpp
@@ -8,7 +8,21 @@ class Solution
 public:
     void sortColors(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i+1; j < nums.size(); j++)
+            {
+                if (nums[i]>nums[j])
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+                
+            }
+            
+        }
+        
     }
 };
 
